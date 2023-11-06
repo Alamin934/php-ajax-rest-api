6a7899ac0afeb1b60827f0e5da677d74e4c47efb
Alamin934 <islamalamin934@gmail.com>
2023-11-06 15:49:43 +0600

    all data fetch using rest api & ajax

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
     //Fetch All Records
+    $.ajax({
+        url: "http://localhost/php_project/php-ajax-rest-api/api-fetch-all.php",
+        type: "GET",
+        success: function(data){
+            if(data.status != false){
+                $.each(data, function(key, value){
+                    $("#load-table").append(`
+                    <tr>
+                        <td class="center">${value.user_id}</td>
+                        <td>${value.user_name}</td>
+                        <td>${value.user_age}</td>
+                        <td>${value.user_city}</td>
+                        <td class="center"><button class="edit-btn" data-eid="${value.user_id}">Edit</button></td>
+                        <td class="center"><button class="delete-btn" data-did="${value.user_id}">Delete</button></td>
+                    </tr>`);
+                });
+            }else{
+                $("#load-table").append(`<tr><td>${data.message}</td></tr>`);
+            }
+        }
+    });
 
     //Insert New Record
 
